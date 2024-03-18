c64f217806e55d674be74272c3b42a06ecd38efc
Olawoyin <yustee2017@gmail.com>
2024-03-18 02:12:56 +0100

    created the working directory for the project

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 import string
-print(string.ascii_uppercase)
+# print(string.ascii_uppercase)
+print(string.ascii_lowercase)
+print(string.digits)
+print(string.ascii_letters)
+print(string.punctuation)
+
+
+x = string.digits
+
+for i in x:
+    print(i)
